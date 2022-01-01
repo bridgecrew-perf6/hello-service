d39b6a24e0b582176adb3933a47585ab63f1af47
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 09:03:34 -0800

    refactor[comments]: remove log statement

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const validateJWT = require("./controllers/leanJwtValidate").verification;
 const tokenExtractor = require("./controllers/tokenExtractor");
 module.exports = function (app) {
     app.get('/', async function (req, res) {
+        // COMPLETE ROUTE. V1.
         res.send('Hello from the other side!');
     });
     app.get('/protected', async function(req, res) {
@@ -12,7 +13,6 @@ module.exports = function (app) {
             logger.info(`${token}`)
             let response = await validateJWT(token);
             if (response !== false) {
-                console.log(response)
                 res.status(200).send(`Hello from the other side ${response["username"]}`)
             } else {
                 res.status(403).json({"message": response})
