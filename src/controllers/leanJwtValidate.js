964afdfb67ac182de8da55c455429bfcc776cc33
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 08:59:06 -0800

    feat[clean]: remove auth files

--- src/controllers/leanJwtValidate.js
+++ src/controllers/leanJwtValidate.js
@@ -4,37 +4,19 @@ const path = require('path');
 const logger = require("./../config/logger").logger;
 
 const publicKeyPath = path.join(__dirname, './../keys/public.key');
-const privateKeyPath = path.join(__dirname, './../keys/private.key');
 
 // key imports
 const publicKEY = fs.readFileSync(publicKeyPath, "utf-8")
-const privateKEY = fs.readFileSync(privateKeyPath, "utf-8")
 
 // token signing options
 const tokenOptions = require("./../config/tokenOptions");
 
 
 module.exports = {
-    sign: async function (username, role) {
-        return new Promise(function (resolve, reject) {
-            jwt.sign({ username: username, role: role }, privateKEY, tokenOptions.signOptions, function (err, signedToken) {
-                if (err) {
-                    logger.error(`Failed to sign token ${err}`);
-                    reject(err);
-                } else {
-                    logger.info(`Signed a token for ${username}`);
-                    resolve(signedToken);
-                }
-            })
-        })
-    },
-
     verification: async function (passedToken) {
         return new Promise(function (resolve, reject) {
             jwt.verify(passedToken, publicKEY, tokenOptions.verifyOptions, function (err, decodedToken) {
                 if (err) {
-                    logger.info(publicKEY)
-                    logger.info(privateKEY)
                     logger.error(`Failed to validate token ${err}`);
                     resolve(false);
                 } else {
