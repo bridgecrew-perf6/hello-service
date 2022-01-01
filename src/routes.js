38a329cec29a37ab5200ba534531c06ceab7a152
sumukshashidhar <sumukuuu@gmail.com>
2022-01-01 08:58:48 -0800

    feat[routes]: delete unnecessary routes

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,25 @@
-const login = require("./controllers/login")
-const register = require("./controllers/registration")
 const logger = require("./config/logger").logger;
-const validateJWT = require("./controllers/jwtSignValidate").verification;
+const validateJWT = require("./controllers/leanJwtValidate").verification;
 const tokenExtractor = require("./controllers/tokenExtractor");
 module.exports = function (app) {
-    app.get('/', function (req, res) {
-        res.send('UP');
+    app.get('/', async function (req, res) {
+        res.send('Hello from the other side!');
     });
-    // TODO: Add JOI Validation
-    app.post('/login', async function(req, res) {
-        // just send the username password combo to the login controller.
-        let response = await login(req.body.username, req.body.password);
-        if (response !== false) {
-            // successful validation and json token
-            res.status(200).json({"token": response})
-
-        } else {
-        res.json({"Error": "Either user does not exist or wrong password."});
-        }
-    });
-
-    app.post('/register', async function(req, res) {
-        let response = await register(req.body.username, req.body.password, "user");
-        if (response !== false) {
-            res.status(200).json({"object": response})
-        } else {res.status(500).json({"Message": "Failed to create user"})}
-        
-    });
-
-    app.post('/validate', async function(req, res) {
+    app.get('/protected', async function(req, res) {
+        // to just check if you can access like JWT pages.
         let token = await tokenExtractor(req.headers.authorization);
         if (token !== false) {
             logger.info(`${token}`)
             let response = await validateJWT(token);
             if (response !== false) {
-                res.status(200).json({"token": response});
+                console.log(response)
+                res.status(200).send(`Hello from the other side ${response["username"]}`)
             } else {
-                res.status(403).json({"message": "decoding failed"})
+                res.status(403).json({"message": response})
             }
+        }
+        else {
+            res.status(403).json({"message": "Failed Extraction"})
         }
     })
     return app;
